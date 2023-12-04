3c42bd523805d1b284f7da5d2288cefc449cff64
richard6094 <ruiqiyang@microsoft.com>
2023-12-04 19:10:17 +0800

    Added 1st mac external script

---
+++ External_script_excel_sample_mac.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if git is installed
+if ! command -v git &> /dev/null
+then
+    echo "Git is not installed, installing now..."
+    brew install git
+else
+    echo "Git is already installed!"
+fi
+
+# Check if Node.js is installed
+if ! command -v node &> /dev/null
+then
+    echo "Node.js is not installed, installing now..."
+    brew install node
+else
+    echo "Node.js is already installed!"
+fi
+
+# Check if Yeoman and generator-office are installed
+if ! npm list -g --depth=0 | grep generator-office &> /dev/null
+then
+    echo "Yeoman Office is not installed, installing now..."
+    sudo npm install -g yo generator-office
+else
+    echo "Yeoman Office has already been installed."
+fi
+
+# Now Yeoman Office has been installed. Create a sample project.
+foldername="Office_sample"
+counter=0
+
+while [ -d "$foldername" ]
+do
+    counter=$((counter + 1))
+    foldername="Office_sample_$counter"
+done
+
+mkdir "$foldername"
+cd "$foldername"
+yo office
