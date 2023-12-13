8012b00a12c4ff37c1445a60fab49f22c23d20a6
richard6094 <ruiqiyang@microsoft.com>
2023-12-13 18:45:32 +0800

    Update mac scripts

--- External_scripts/External_script_word_sample_mac.sh
+++ External_scripts/External_script_word_sample_mac.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
- 
+
 # Check if git is installed
 if ! command -v git &> /dev/null
 then
-    echo "Git is not installed, installing now..."
-    brew install git
+    # echo "Git is not installed, installing now..."
+    # brew install git
+    echo "Git is not installed, please install git first."
+    exit
 else
     echo "Git is already installed!"
 fi
@@ -12,42 +14,58 @@ fi
 # Check if Node.js is installed. If Node is not installed, check homebrew/install homebrew and Node.js 18.
 if ! command -v node &> /dev/null
 then
-    echo "Node.js is not installed, installing Node.js using homebrew now..."
+    # echo "Node.js is not installed, installing Node.js using homebrew now..."
+    echo "Node.js is not installed, please install Node.js first."
+    exit
 
-    # Check if Homebrew is installed
-    if ! command -v brew &> /dev/null
-    then
-        echo "Homebrew is not installed, installing now..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
-        echo "Homebrew is already installed!"
-    fi
+    # # Check if Homebrew is installed
+    # if ! command -v brew &> /dev/null
+    # then
+    #     echo "Homebrew is not installed, installing now..."
+        
+    #     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    #     exit_status=$?
+    #     echo "Exit status of Installing homebrew: $exit_status"
 
-    brew install node@18
-    brew link --overwrite --force node@18
+    #     if [ $exit_status -ne 0 ]; then
+    #         echo "An error occurred while running the command. Trying to fix it..."
+    #         sudo chown -R $(whoami) /usr/local/share/zsh /usr/local/share/zsh/site-functions
+    #     fi
+    # else
+    #     echo "Homebrew is already installed!"
+    # fi
+
+    # brew install node@16
+    # brew link --overwrite --force node@16
 else
     echo "Node.js is already installed!"
-    #check the version of Node.js
+    # check the version of Node.js
     NODE_VERSION=$(node -v)
-    if [[ "$NODE_VERSION" != "v16"*  && "$NODE_VERSION" != "v18"* ]]
-    then
-        echo "The current version of Node.js is not 16 or 18, installing Node.js 18 now..."
-        brew install node@18
-        brew link --overwrite --force node@18
-    fi
+    # if [[ "$NODE_VERSION" != "v16"* ]]
+    # then
+    #     echo "The current version of Node.js is not 16 or 18, installing Node.js 18 now..."
+    #     brew install node@16
+    #     brew link --overwrite --force node@16
+    #     exit_status=$?
+    #     if [ $exit_status -ne 0 ]; then
+    #         echo "An error occurred while linking node. Trying to fix it..."
+    #         sudo chown -R $(whoami) /usr/local
+    #     fi
+    # fi
 fi
- 
+
+
 # Check the version of Node.js
 echo "The current version of Node.js is: $(node -v)"
  
-# check if typescript & tsc have been installed
-if ! command -v tsc &> /dev/null
-then
-    echo "TypeScript is not installed, installing now..."
-    npm install -g typescript
-else
-    echo "TypeScript is already installed!"
-fi
+# # check if typescript & tsc have been installed
+# if ! command -v tsc &> /dev/null
+# then
+#     echo "TypeScript is not installed, installing now..."
+#     sudo npm install -g typescript
+# else
+#     echo "TypeScript is already installed!"
+# fi
  
 # # Check the version of npm
 # NPM_VERSION=$(npm -v)
@@ -65,7 +83,7 @@ fi
 if ! npm list -g --depth=0 | grep office_addin_sample_scripts &> /dev/null
 then
     echo "office_addin_sample_scripts is not installed, installing now..."
-    npm install -g office_addin_sample_scripts
+    sudo npm install -g office_addin_sample_scripts
 else
     echo "office_addin_sample_scripts has already been installed."
 fi
@@ -88,5 +106,7 @@ if [ -n "$pid" ]; then
 else
     echo "Port 3000 is not in use."
 fi
- 
+
+# sudo chown -R $(whoami) ~/.npm
+
 office_addin_sample_scripts launch word_aigc $foldername
